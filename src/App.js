269ae65e0cfcba9974d8a2a33a90d3c00de71d6a
Valerie Kwan <valeriekwan@Valeries-MacBook-Air.local>
2024-04-28 13:45:53 -0700

    integration

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './components/homepage.js'
+import WebcamCapture from './components/webcam';
 
 // export const routes = [{path: '/', name: 'Home', component: <Home />}];
 function App() {
   return (
-  <Home />
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <div className="App">
+      <header className="App-header">
+        <WebcamCapture />
+      </header>
+    </div>
   );
 }
 
---
+++ src/components/webcam.js
@@ -0,0 +1,46 @@
+import Webcam from "react-webcam";
+import React, {useState, useRef, useCallback } from "react";
+
+const WebcamCapture = () => {
+    const webcamRef = useRef(null);
+    const [currentState, setCurrentState] = useState("capturing");
+    const [imgSrc, setImgSrc] = useState(null);
+    /**
+     const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImgSrc(imageSrc);
+  }, [webcamRef]);
+     */
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImgSrc(imageSrc);
+  }, [webcamRef, setImgSrc]);
+    const webCamButton = () => {
+        if (currentState === "capturing") {
+            capture();
+            setCurrentState("screenshot");
+        } else if (currentState === "screenshot") {
+            setCurrentState("capturing");
+        }
+      };
+    
+    return (
+      <>
+      <button onClick={webCamButton}>
+      {currentState === "capturing" ? (
+          <Webcam
+            audio={false}
+            mirrored={true}
+            height={600}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            width={1280}
+          />
+        ) : (
+          <img src={imgSrc} />
+        )}
+        </button>
+      </>
+    );
+  };
+  export default WebcamCapture;
