b04234e61134f6277d88b0bbd39290a7716ebddd
sonali-7 <99060458+sonali-7@users.noreply.github.com>
2024-04-28 13:31:09 -0700

    works ig?

--- src/webcam.js
+++ src/webcam.js
@@ -1,39 +1,44 @@
 import Webcam from "react-webcam";
-import React, {useRef, useCallback } from "react";
+import React, {useState, useRef, useCallback } from "react";
 
 const WebcamCapture = () => {
     const webcamRef = useRef(null);
+    const [currentState, setCurrentState] = useState("capturing");
+    const [imgSrc, setImgSrc] = useState(null);
     /**
      const capture = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
     setImgSrc(imageSrc);
   }, [webcamRef]);
      */
-    const currentState = 'capturing'
-    const capture = () => {
-        if (currentState == 'capturing') {
-          useCallback(
-            () => {
-              const imageSrc = webcamRef.current.getScreenshot();
-            },
-            [webcamRef]
-          );
-          currentState = 'screenshot';
-        } else if (currentAction == 'screenshot') {
-          currentState = 'capturing';
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImgSrc(imageSrc);
+  }, [webcamRef, setImgSrc]);
+    const webCamButton = () => {
+        if (currentState === "capturing") {
+            capture();
+            setCurrentState("screenshot");
+        } else if (currentState === "screenshot") {
+            setCurrentState("capturing");
         }
       };
+    
     return (
       <>
-      <button onClick={capture}>
-        <Webcam
-          audio={false}
-          mirrored={true}
-          height={600}
-          ref={webcamRef}
-          screenshotFormat="image/jpeg"
-          width={1280}
-        />
+      <button onClick={webCamButton}>
+      {currentState === "capturing" ? (
+          <Webcam
+            audio={false}
+            mirrored={true}
+            height={600}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            width={1280}
+          />
+        ) : (
+          <img src={imgSrc} />
+        )}
         </button>
       </>
     );
