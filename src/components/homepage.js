6fc0f543e6dcae128d4299c208e4ae7c31484c72
Valerie Kwan <valeriekwan@Valeries-MacBook-Air.local>
2024-04-28 18:21:21 -0700

    WORKSSSS AHDSAHDJAKDFKAD

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -7,66 +7,30 @@ const Home = () => {
     const { imgSrc, setImgSrc } = useContext(ImgContext);
     const genAI = new GoogleGenerativeAI('AIzaSyA8xpePRCL_zqFgdYZH7BisU79NL5g0JgE');
     const [data, setResponse] = useState('');
-
-    async function fileToGenerativePart(imgSrc) {
-        const base64EncodedDataPromise = new Promise((resolve) => {
-          const reader = new FileReader();
-          reader.onloadend = () => resolve(reader.result.split(',')[1]);
-          reader.readAsDataURL(imgSrc);
-        });
-        return {
-          inlineData: { data: await base64EncodedDataPromise, mimeType: imgSrc.type },
-        };
-      }
       
       async function run() {
         const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
 
         const prompt = "tell me what in this image";
       
-      const image = {
-          inlineData: {
-            data: imgSrc,
-            mimeType: "image/png",
-          },
-        };
-  
-        const result = await model.generateContent([prompt, image]);
+        let tempImageObject = {
+            inlineData: {
+            data: imgSrc.substring(imgSrc.indexOf(",")+1),
+            mimeType: imgSrc.substring(imgSrc.indexOf(":")+1, imgSrc.indexOf(";"))
+             }
+        }
+        const result = await model.generateContent([prompt, tempImageObject]);
         const response = await result.response;
         const text = response.text();
         setResponse(text);
       }
-  
-  
       useEffect(() => {
-        run() 
-      }, [data]) 
-    /*
-    const handleImageChange = (e) => {
-        const file = e.target.files[0];
-
-        getBase64(file).then((result) => {
-            setImage(result);
-        }).catch(e => console.log(e))
-
-        fileToGenerativePart(file).then((image) => {
-            setImageInlineData(image);
-        });
-    }
-
-    async function fileToGenerativePart(file) {
-        const base64EncodedDataPromise = new Promise((resolve) => {
-            const reader = new FileReader();
-            reader.onloadend = () => resolve(reader.result.split(',')[1]);
-            reader.readAsDataURL(file);
-        });
-        
-        return {
-            inlineData: { data: await base64EncodedDataPromise, mimeType: file.type },
-        };
-    }
-    */
-    
+        // Check if imgSrc has changed
+        if (imgSrc !== null) {
+          run();
+        }
+      }, [imgSrc]) 
+  
     return (
         <div>
         <p>Generated Text {data}</p>
